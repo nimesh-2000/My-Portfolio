fe7f5368de7917dfb3d40a851e0ff16ef81a106a
nimesh-2000 <nimeshdenuwan2000@gmail.com>
2022-10-10 22:05:22 +0530

    add index controller

---
+++ assignments/JS/SPA/controller/indexController.js
@@ -0,0 +1,31 @@
+$("#cForm").css('display','none');
+$("#iForm").css('display','none');
+$("#oForm").css('display','none');
+
+$("#h").click(function(){
+    $("#cForm").css('display','none');
+    $("#dashboardContent").css('display','block');
+    $("#iForm").css('display','none');
+    $("#oForm").css('display','none');
+});
+
+$("#c").click(function(){
+    $("#cForm").css('display','block');
+    $("#dashboardContent").css('display','none');
+    $("#iForm").css('display','none');
+    $("#oForm").css('display','none');
+});
+
+$("#i").click(function(){
+    $("#cForm").css('display','none');
+    $("#dashboardContent").css('display','none');
+    $("#iForm").css('display','block');
+    $("#oForm").css('display','none');
+});
+
+$("#o").click(function(){
+    $("#cForm").css('display','none');
+    $("#dashboardContent").css('display','none');
+    $("#iForm").css('display','none');
+    $("#oForm").css('display','block');
+});
