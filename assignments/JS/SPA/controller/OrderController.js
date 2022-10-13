3642e111eac4722a196905063aa5091c5a3e1240
nimesh-2000 <nimeshdenuwan2000@gmail.com>
2022-10-14 00:37:01 +0530

    add customer and item listners

--- assignments/JS/SPA/controller/OrderController.js
+++ assignments/JS/SPA/controller/OrderController.js
@@ -1,6 +1,6 @@
 function loadAllCustomerId() {
     alert("load all")
-    // $('#oCustomerId').empty();
+    $('#oCustomerId').empty();
     for (let cus of customers) {
         $('#oCustomerId').append(`<option>${cus.id}</option>`);
     }
@@ -13,16 +13,27 @@ function loadAllItemCodes() {
     }
 }
 
-// /*Listener fir the Customer Combo*/
-// $('#oCustomerId').on('change',function (){
-//     /*get Customer*/
-//     let customer = searchCustomer($('#oCustomerId').val());
-//
-//     $('#oCustomerName').val(customer.name);
-//     $('#oCustomerAddress').val(customer.address);
-//     $('#oCustomerPhone').val(customer.phone);
-//
-//
-// });
+
+$('#oCustomerId').on('change',function (){
+    /*get Customer*/
+    let customer = searchCustomer($('#oCustomerId').val());
+
+    $('#oCustomerName').val(customer.name);
+    $('#oCustomerAddress').val(customer.address);
+    $('#oCustomerPhone').val(customer.contact);
+
+
+});
+
+
+$('#oItemId').on('change',function (){
+
+    let item = searchItem($('#oItemId').val());
+
+    $('#oItemName').val(item.name);
+    $('#oItemPrice').val(item.price);
+    $('#qtyOnHand').val(item.quantity);
+
+});
 
 
